a29e9d675c60ba522dd887d622a47132be597369
Om Morendha <omkmorendha@gmail.com>
2024-09-09 12:55:59 +0530

    Add checklist

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ WEBHOOK_SECRET = os.environ.get("WEBHOOK_SECRET")
 TO_EMAIL = ast.literal_eval(os.environ.get("TO_EMAIL"))
 
 bot = TeleBot(BOT_TOKEN, threaded=True)
-bot.remove_webhook()
-time.sleep(1)
-bot.set_webhook(url=f"{URL}/{WEBHOOK_SECRET}")
+# bot.remove_webhook()
+# time.sleep(1)
+# bot.set_webhook(url=f"{URL}/{WEBHOOK_SECRET}")
 r = Redis(connection_pool=pool)
 
 
@@ -125,87 +125,88 @@ def get_age_from_gpt(message):
         return None
 
 
-def age_less_than_1(message):
-    """Handler for children under 1 month old."""
+checklist_options = {
+    3 : ["Alerts to sound.", "Quiets or smiles when you talk.", "Makes sounds back and forth with you.", "Makes sounds that differ depending on whether they are happy or upset.", "Coos, makes sounds like ooooo, aahh, and mmmmm.", "Recognizes loved ones and some common objects.", "Turns or looks toward voices or people talking."],
+    6 : ["Giggles and laughs.", "Responds to facial expressions.", "Looks at objects of interest and follows objects with their eyes.", "Reacts to toys that make sounds, like those with bells or music.", " Vocalizes during play or with objects in mouth.", "Vocalizes different vowel sounds—sometimes combined with a consonant—like uuuuuummm, aaaaaaagoo, or daaaaaaaaaa.", "Blows 'raspberries.'"],
+    9 : ["Looks at you when you call their name.", "Stops for a moment when you say, 'No.'", "Babbles long strings of sounds, like mamamama, upup, or babababa.", "Looks for loved ones when upset.", "Raises arms to be picked up.", "Recognizes the names of some people and objects.", "Pushes away unwanted objects."],
+    12 : ["By age 10 months, reaches for objects.", "Points, waves, and shows or gives objects.", "Imitates and initiates gestures for engaging in social interactions and playing games, like blowing kisses or playing peek-a-boo.", "Tries to copy sounds that you make.", " Enjoys dancing.", "Responds to simple words and phrases like 'Go bye-bye' and 'Look at Mommy.'", "Says one or two words—like mama, dada, hi, and bye."],
+    18 : ["Looks around when asked 'where' questions—like 'Where's your blanket?'", "Follows directions—like 'Give me the ball', 'Hug the teddy bear', 'Come here', or 'Show me your nose.'", "Points to make requests, to comment, or to get information.", "shakes head for 'no' and nods head for 'yes.'", "Understands and uses words for common objects, some actions, and people in their lives", "Identifies one or more body parts.", "Uses gestures when excited, like clapping or giving a high-five, or when being silly, like sticking out their tongue or making funny faces.", "Uses a combination of long strings of sounds, syllables, and real words with speech-like inflection."],
+    24 : ["Uses and understands at least 50 different words for food, toys, animals, and body parts. Speech may not always be clear—like du for 'shoe' or dah for 'dog.'", "Puts two or more words together—like more water or go outside.", "Follows two-step directions—like 'Get the spoon, and put it on the table.'", "Uses words like me, mine, and you.", " Uses words to ask for help.", "Uses possessives, like Daddy's sock."],
+    36 : ["Uses word combinations often but may occasionally repeat some words or phrases, like baby - baby - baby sit down or I want - I want juice.", "Tries to get your attention by saying, Look at me!", "Says their name when asked.", "Uses some plural words like birds or toys.", "Uses -ing verbs like eating or running. Adds -ed to the end of words to talk about past actions, like looked or played.", " Gives reasons for things and events, like saying that they need a coat when it's cold outside.", "Asks why and how.", "Answers questions like 'What do you do when you are sleepy?' or 'Which one can you wear?'", "Correctly produces p, b, m, h, w, d, and n in words.", "Correctly produces most vowels in words.", "Speech is becoming clearer but may not be understandable to unfamiliar listeners or to people who do not know your child."],
+    48 : ["Compares things, with words like bigger or shorter.", "Tells you a story from a book or a video.", "Understands and uses more location words, like inside, on, and under.", "Uses words like a or the when talking, like a book or the dog.", "Pretends to read alone or with others.", "Recognizes signs and logos like STOP.", "Pretends to write or spell and can write some letters.", "Correctly produces t, k, g, f, y, and -ing in words.", "Says all the syllables in a word.", "Says the sounds at the beginning, middle, and end of words.", "By age 4 years, your child talks smoothly. Does not repeat sounds, words, or phrases most of the time.", "By age 4 years, your child speaks so that people can understand most of what they say. Child may make mistakes on sounds that are later to develop—like l, j, r, sh, ch, s, v, z, and th.", "By age 4 years, your child says all sounds in a consonant cluster containing two or more consonants in a row—like the tw in tweet or the -nd in sand. May not produce all sounds correctly—for example, spway for 'spray.'"],
+    60 : ["Produces grammatically correct sentences. Sentences are longer and more complex.", " Includes (1) main characters, settings, and words like and to connect information and (2) ideas to tell stories.", "Uses at least one irregular plural form, like feet or men.", " Understands and uses location words, like behind, beside, and between.", "Uses more words for time—like yesterday and tomorrow—correctly.", "Follows simple directions and rules to play games.", "Locates the front of a book and its title.", "Recognizes and names 10 or more letters and can usually write their own name.", "Imitates reading and writing from left to right.", "Blends word parts, like cup + cake = cupcake. Identifies some rhyming words, like cat and hat.", "Produces most consonants correctly, and speech is understandable in conversation."],
+}
+
+
+def create_checklist_markup(user_id, checklist_options):
+    """Creates a checklist markup with current selections."""
+    user_data = ast.literal_eval(r.get(user_id).decode("utf-8"))
+    
+    if 'checklist' not in user_data:
+        user_data['checklist'] = [False] * len(checklist_options)
+        r.set(user_id, str(user_data))
+
+    markup = types.InlineKeyboardMarkup()
+    for idx, option in enumerate(checklist_options):
+        status = "✅" if user_data['checklist'][idx] else "⬜️"
+        markup.add(types.InlineKeyboardButton(f"{status} {option}", callback_data=f"toggle_{idx}"))
+
+    markup.add(types.InlineKeyboardButton("Submit", callback_data="submit_checklist"))
+    restart_button = types.InlineKeyboardButton("Restart", callback_data="restart")
+    markup.add(restart_button)
+    return markup
+
+
+def checklist(message, checklist_options):
+    """Send checklist to the user with toggle options."""
     try:
+        print('hello')
         msg = bot.send_message(
             message.chat.id,
-            "We are sorry, but our system only supports children atleast 1 month old.",
+            "Please select the options by checking or unchecking:",
+            reply_markup=create_checklist_markup(message.from_user.id, checklist_options)
         )
     except Exception as e:
-        logger.error(f"Error handling age less than 1: {e}")
+        logger.error(f"Error in sending checklist: {e}")
 
 
-def age_less_than_3(message):
-    """Handler for children under 3 months old."""
+@bot.callback_query_handler(func=lambda call: call.data.startswith("toggle_"))
+def toggle_checklist(call):
+    """Toggle the checked/unchecked state of an option."""
     try:
-        pass
+        user_id = call.from_user.id
+        option_idx = int(call.data.split("_")[1])
+        user_data = ast.literal_eval(r.get(user_id).decode("utf-8"))
+        age_group = user_data['age_group']
+
+        user_data['checklist'][option_idx] = not user_data['checklist'][option_idx]
+        r.set(user_id, str(user_data))
+
+        bot.edit_message_reply_markup(
+            call.message.chat.id,
+            call.message.message_id,
+            reply_markup=create_checklist_markup(user_id, checklist_options[age_group])
+        )
+
     except Exception as e:
-        logger.error(f"Error handling age less than 3: {e}")
+        logger.error(f"Error toggling checklist: {e}")
 
 
-def age_less_than_6(message):
-    """Handler for children under 6 months old."""
+@bot.callback_query_handler(func=lambda call: call.data == "submit_checklist")
+def submit_checklist(call):
+    """Handle checklist submission."""
     try:
-        pass
+        user_id = call.from_user.id
+        user_data = ast.literal_eval(r.get(user_id).decode("utf-8"))
+
+        selected_options = [option for idx, option in enumerate(checklist_options) if user_data['checklist'][idx]]
+
+        if selected_options:
+            bot.send_message(call.message.chat.id, f"You selected: {', '.join(selected_options)}")
+        else:
+            bot.send_message(call.message.chat.id, "You didn't select any options.")
     except Exception as e:
-        logger.error(f"Error handling age less than 6: {e}")
-
-
-def age_less_than_9(message):
-    """Handler for children under 9 months old."""
-    try:
-        pass
-    except Exception as e:
-        logger.error(f"Error handling age less than 9: {e}")
-
-
-def age_less_than_12(message):
-    """Handler for children under 12 months old."""
-    try:
-        pass
-    except Exception as e:
-        logger.error(f"Error handling age less than 12: {e}")
-
-
-def age_less_than_18(message):
-    """Handler for children under 18 months old."""
-    try:
-        pass
-    except Exception as e:
-        logger.error(f"Error handling age less than 18: {e}")
-
-
-def age_less_than_24(message):
-    """Handler for children under 24 months old."""
-    try:
-        pass
-    except Exception as e:
-        logger.error(f"Error handling age less than 24: {e}")
-
-
-def age_less_than_36(message):
-    """Handler for children under 36 months old."""
-    try:
-        pass
-    except Exception as e:
-        logger.error(f"Error handling age less than 36: {e}")
-
-
-def age_less_than_48(message):
-    """Handler for children under 48 months old."""
-    try:
-        pass
-    except Exception as e:
-        logger.error(f"Error handling age less than 48: {e}")
-
-
-def age_less_than_60(message):
-    """Handler for children under 60 months old."""
-    try:
-        pass
-    except Exception as e:
-        logger.error(f"Error handling age less than 60: {e}")
+        logger.error(f"Error submitting checklist: {e}")
 
 
 def age_more_than_60(message):
@@ -245,29 +246,33 @@ def get_child_age(message):
         r.set(message.from_user.id, str(user_data))
         bot.send_message(message.chat.id, f"Child's name and age saved: {user_data}", parse_mode="Markdown")
 
-        if age < 1:
-            bot.register_next_step_handler(msg, age_less_than_1)
-        elif age <= 3:
-            bot.register_next_step_handler(msg, age_less_than_3)
+        if age <= 3:
+            age_group = 3
         elif age <= 6:
-            bot.register_next_step_handler(msg, age_less_than_6)
+            age_group = 6
         elif age <= 9:
-            bot.register_next_step_handler(msg, age_less_than_9)
+            age_group = 9
         elif age <= 12:
-            bot.register_next_step_handler(msg, age_less_than_12)
+            age_group = 12
         elif age <= 18:
-            bot.register_next_step_handler(msg, age_less_than_18)
+            age_group = 18
         elif age <= 24:
-            bot.register_next_step_handler(msg, age_less_than_24)
+            age_group = 24
         elif age <= 36:
-            bot.register_next_step_handler(msg, age_less_than_36)
+            age_group = 36
         elif age <= 48:
-            bot.register_next_step_handler(msg, age_less_than_48)   
+            age_group = 48 
         elif age <= 60:
-            bot.register_next_step_handler(msg, age_less_than_60)   
+            age_group = 60  
         else:
             bot.register_next_step_handler(msg, age_more_than_60)
     
+        user_data = ast.literal_eval(r.get(message.from_user.id).decode("utf-8"))
+        user_data["age_group"] = age_group
+        r.set(message.from_user.id, str(user_data))    
+
+        print('weg')
+        checklist(message, checklist_options[age_group])
     except ValueError:
         msg = bot.send_message(message.chat.id, "Invalid age. Please enter a valid age.")
         bot.register_next_step_handler(msg, get_child_age)
